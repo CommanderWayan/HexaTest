327c1a0467d3dc5e0e3858054d4bd3564d0c978e
CommanderWayan <wayan-mail@gmx.net>
2011-10-11 00:44:47 +0200

    cleanup

---
+++ HexaTest/entities/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HexaTest.entities
+{
+	class Weapon
+	{
+		int _minFireRange;
+		int _maxFireRange;
+		int _actualAmmo;
+		int _maxAmmo;
+
+		//TODO: Maxammo setzen!
+		public Weapon(int ActualAmmo)
+		{
+			this._actualAmmo = ActualAmmo;
+		}
+	}
+}
